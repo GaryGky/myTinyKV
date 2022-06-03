0c72f844b60d0deeb3d8fd57a96b49d42800df2a
kaiyuan.gan <kaiyuan.gan@bytedance.com>
2022-06-03 21:42:37 +0800

    implement storage layer's read / write function

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,38 +1,57 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"log"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
-	// Your Data Here (1).
+	engine *engine_util.Engines
 }
 
+// NewStandAloneStorage 初始化一个数据库实例: In-memory True
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	db, err := badger.Open(badger.Options{
+		Dir:      conf.StoreAddr,
+		ValueDir: conf.DBPath,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engine := engine_util.NewEngines(db, nil, conf.DBPath, "")
+
+	return &StandAloneStorage{engine: engine}
 }
 
 func (s *StandAloneStorage) Start() error {
-	// Your Code Here (1).
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
-	// Your Code Here (1).
 	return nil
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
-	// Your Code Here (1).
-	return nil, nil
+	memReader, err := storage.NewMemStorage().Reader(ctx)
+	return memReader, err
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
-	// Your Code Here (1).
-	return nil
+	wb := &engine_util.WriteBatch{}
+	for _, modify := range batch {
+		wb.SetCF(modify.Cf(), modify.Key(), modify.Value())
+	}
+	err := s.engine.WriteKV(wb)
+	if err != nil {
+		log.Fatalf("StandAlone.Write, error: %v\n", err)
+	}
+	return err
 }
